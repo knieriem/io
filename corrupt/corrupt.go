b2f3e0ec7b83aae69844628a99aaad2d4afa9edf
Michael Teichgräber <mteichgraeber@gmx.de>
2017-08-01 12:26:43 +0200

    corrupt: make it usable again; wrap ReadWriter, not RWCloser

--- corrupt/corrupt.go
+++ corrupt/corrupt.go
@@ -3,38 +3,54 @@ package corrupt
 import (
 	"io"
 	"time"
+	"math/rand"
 )
 
+func New(f io.ReadWriter) io.ReadWriter {
+	rw := &corruptingWrap{rw: f}
+	rw.corrupt.nmsgMax = time.Now().Nanosecond() & 7
+	return rw
+}
+
+func Wrap(f io.ReadWriter, _ string) io.ReadWriter {
+	return New(f)
+}
+
 type corruptingWrap struct {
-	io.ReadWriteCloser
+	rw      io.ReadWriter
 	corrupt struct {
 		tPrevW time.Time
 		tPrevR time.Time
 		n      int
+		nmsg int
+		nmsgMax int
 	}
 }
 
 func (w *corruptingWrap) Read(buf []byte) (n int, err error) {
 retry:
-	n, err = w.ReadWriteCloser.Read(buf)
+	n, err = w.rw.Read(buf)
 	if err == nil && n > 0 {
 		if corrupt.fakeRxActive() {
 			n = corrupt.copyRx(buf)
 			return
 		}
 		t := time.Now()
-		if Δt := t.Sub(w.corrupt.tPrevR); Δt > 2000*time.Millisecond {
+		if Δt := t.Sub(w.corrupt.tPrevR); w.corrupt.nmsg > w.corrupt.nmsgMax || w.corrupt.nmsg > 0 && Δt > 2000*time.Millisecond {
 			if (w.corrupt.n & 3) == 1 {
 				buf[n-1]++
 			} else {
 				n--
 			}
+			w.corrupt.nmsg = 0;
+			w.corrupt.nmsgMax = 3+rand.Intn(17)
 			w.corrupt.tPrevR = t
 			if n == 0 {
 				goto retry
 			}
 		}
 	}
+	w.corrupt.nmsg++
 	w.corrupt.n++
 	return
 }
@@ -52,7 +68,7 @@ func (w *corruptingWrap) Write(buf []byte) (n int, err error) {
 	}
 	w.corrupt.n++
 
-	n, err = w.ReadWriteCloser.Write(buf)
+	n, err = w.rw.Write(buf)
 	n += dn
 	return
 }
